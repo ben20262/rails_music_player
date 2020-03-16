65d87ebbf0a5f31b53303ddf69b99eed4a0e48a3
ben20262 <ben20262@aim.com>
2020-03-16 19:04:58 -0400

    route creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :artists
+  resources :genres, only: [:new, :create, :show, :index]
+  resources :playlists
+  resources :sessions, only: [:new, :create]
+  resources :songs
+  resources :users, only: [:new, :create, :show]
 end
