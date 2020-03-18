ca22e0edc1089a59bd713d7530b593655985c051
ben20262 <ben20262@aim.com>
2020-03-18 19:26:19 -0400

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:new, :create, :show]
+  root to: 'users#show'
+  resources :users, only: [:new, :create]
+  resources :users, only: [:show] do
+    resources :songs, only: [:index, :show, :create]
+  end
 
   resources :artists, only: [:new, :create, :update, :show, :index, :destroy]
   resources :genres, only: [:new, :create, :show, :index, :destroy]
-  resources :playlists, only: [:new, :create, :index, :show, :update, :destroy]
   resources :songs
 
-  root to: 'users#show'
+  resources :playlists, only: [:new, :create, :index, :update, :destroy]
+  resources :playlists, only: [:show] do
+    resources :songs, only: [:index, :show]
+  end
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy', as: 'logout'
   get '/signup', to: 'users#new', as: 'signup'
-
-  get '/users/:id/songs', to: 'users#songs', as: 'user_songs'
-  post '/users/songs/:id', to: 'users#add_song', as: 'add_user_song'
 
   get '/songs/:id/playlist', to: 'songs#add_song_playlist', as: 'add_playlist_song'
   patch '/playlists/:id/songs/:song_id', to: 'playlists#update', as: 'update_playlist'
